55862c8672b55e82ade7377f70f547e0c42454e1
mjsb05 <maurijsb25@gmail.com>
2025-03-29 21:28:25 -0600

    ListView

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -17,6 +17,24 @@ class ChatScreen extends StatelessWidget {
         title: const Text('Solecito ☀️'),
         centerTitle: false,
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(children: [
+          Expanded(child: ListView.builder(itemBuilder: (context, index) {
+            return Text('Indice: $index');
+          })),
+          Text('Mundo ')
+        ]),
+      ),
     );
   }
 }
